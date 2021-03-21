d4901a13da468d59475fea6ac3a6334a1a8d1c8c
Crowdbotics <team@crowdbotics.com>
2021-03-21 19:33:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dangrek_app_25170/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dangrek_app_25170;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dangrek_app_25170";
+    }
+}
---
+++ src/config/app.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  // todo add library to handle env variables
+  emailAuthAPIEndPoint: "https://dangrek-app-25170.botics.co",
+  defaultTimeout: 5000
+};
